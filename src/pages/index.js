e6e85f9ccd270b11b40d4c237fe1a0ad2f16c59d
BC91328 <rakshith.m@macys.com>
2022-04-28 11:58:12 +0530

    login

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,20 @@
 import * as React from "react"
+import MicrosoftLogin from "react-microsoft-login"
 
+const IndexPage = () => {
+  const authHandler = (err, data) => {
+    console.log(err, data)
+  }
 
-const IndexPage = () => (
-  <div>
-    <h1>Arch Neo</h1>
-  </div>
-  // <Layout>
-  //   <Seo title="Home" />
-  //   Hi Arch Neo Team 1
- 
-   
-  // </Layout>
-)
+  return (
+    <div>
+      <h4>Arch Neo</h4>
+      <MicrosoftLogin
+        clientId={"8c1ce88c-c0a0-4cc5-837f-baed7baf91ef"}
+        authCallback={authHandler}
+      />
+    </div>
+  )
+}
 
 export default IndexPage
