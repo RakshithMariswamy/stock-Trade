20eeba72ce9c42f24da42a5990346d15021e1dd9
BC91328 <rakshith.m@macys.com>
2022-04-28 11:30:33 +0530

    build

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,16 @@
 import * as React from "react"
-import MicrosoftLogin from "react-microsoft-login"
-import Layout from "../components/layout"
-import Seo from "../components/seo"
 
-const IndexPage = () => {
-  const authHandler = (err, data) => {
-    console.log(err, data)
-  }
 
-  const clientId = "8c1ce88c-c0a0-4cc5-837f-baed7baf91ef";
-
-  return (
-
-     <Layout>
-    <Seo title="Home" />
-    Hi Arch Neo Team 1
-    <MicrosoftLogin clientId={clientId} authCallback={authHandler} />
-  </Layout>
-    // <nav
-    //   style={{
-    //     display: "flex",
-    //     padding: "20px",
-    //     borderBottom: "1px solid blue",
-    //   }}
-    // >
-    //  <Layout>
-    //    <Seo title="Home"/>
-    //   <div style={{ float: "right", padding: "20px" }}>
-       
-    //   </div>
-    //   </Layout>
-    // </nav>
-  )
-
+const IndexPage = () => (
+  <div>
+    <h1>Arch Neo</h1>
+  </div>
+  // <Layout>
+  //   <Seo title="Home" />
+  //   Hi Arch Neo Team 1
  
-}
+   
+  // </Layout>
+)
 
 export default IndexPage
