24bc04f22e97971ecbaa4abbdc63197c3d6bbcd9
BC91328 <rakshith.m@macys.com>
2022-05-02 10:54:38 +0530

    login

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,30 @@
-import * as React from "react"
-// import MicrosoftLogin from "react-microsoft-login"
+import React, { useState } from "react"
+import MicrosoftLogin from "react-microsoft-login"
 
 const IndexPage = () => {
-  // const authHandler = (err, data) => {
-  //   console.log(err, data)
-  // }
+  const [msalInstance, onMsalInstanceChange] = useState()
+  const loginHandler = (err, data, msal) => {
+    console.log(err, data)
+    // some actions
+    if (!err && data) {
+      onMsalInstanceChange(msal)
+    }
+  }
+
+  const logoutHandler = () => {
+    msalInstance.logout()
+  }
 
   return (
     <div>
       <h4>Arch Neo</h4>
-      {/* <MicrosoftLogin
-        clientId={"8c1ce88c-c0a0-4cc5-837f-baed7baf91ef"}
-        authCallback={authHandler}
-      /> */}
+      {msalInstance && <button onClick={logoutHandler}>Logout</button>}
+      {!msalInstance && (
+        <MicrosoftLogin
+          clientId={"8c1ce88c-c0a0-4cc5-837f-baed7baf91ef"}
+          authCallback={loginHandler}
+        />
+      )}
     </div>
   )
 }
