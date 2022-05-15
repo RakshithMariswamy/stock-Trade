e2f63c93dc3921055c887b22333217ab0107cf00
BC91328 <rakshith.m@macys.com>
2022-05-15 22:08:08 +0530

    fixes

--- src/functions/batch.js
+++ src/functions/batch.js
@@ -1,6 +1,6 @@
 import BatchMock from "../mock/batch.json"
 exports.handler = async function (event, context) {
-    const batchInfo = BatchMock.companyInfo
+    const batchInfo = BatchMock.batchInfo
     return {
       statusCode: 200,
       body: JSON.stringify(batchInfo),
--- src/functions/group.js
+++ src/functions/group.js
@@ -1,6 +1,6 @@
 import GroupComanyMock from "../mock/group.json"
 exports.handler = async function (event, context) {
-  const groupMockInfo = GroupComanyMock.companyInfo
+  const groupMockInfo = GroupComanyMock.groupInfo
   return {
     statusCode: 200,
     body: JSON.stringify(groupMockInfo),
