11d032d0756cdbb93f737f8b268c8c62e3da7862
BC91328 <rakshith.m@macys.com>
2022-05-26 11:13:09 +0530

    fixes

--- src/functions/company.js
+++ src/functions/company.js
@@ -1,11 +1,12 @@
+import fetch from "cross-fetch"
 exports.handler = async function (event, context) {
-    const companyURL = 'https://stock-app-priyesh-heroku.herokuapp.com/stock'
-    console.log(companyURL)
-     const response = await fetch(companyURL)
-     const data = await response.json();
-     console.log(data);
-     return { 
-         statusCode: 200,
-         body: JSON.stringify({data})
-     }
+  const companyURL = "https://stock-app-priyesh-heroku.herokuapp.com/stock"
+  console.log(companyURL)
+  const response = await fetch(companyURL)
+  const data = await response.json()
+  console.log(data)
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ data }),
   }
+}
