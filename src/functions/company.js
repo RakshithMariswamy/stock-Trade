a51e805fb7405a07c530c86d569090e760691cf4
BC91328 <rakshith.m@macys.com>
2022-05-26 10:50:05 +0530

    api

--- src/functions/company.js
+++ src/functions/company.js
@@ -1,4 +1,15 @@
 exports.handler = async function (event, context) {
-  const companyURL = 'https://stock-app-priyesh-heroku.herokuapp.com/stock'
-    return await fetch(companyURL)
+  try{
+    const companyURL = 'https://stock-app-priyesh-heroku.herokuapp.com/stock'
+     const response = await fetch(companyURL)
+     const data = await response.json();
+     console.log(data);
+     return { 
+         statusCode: 200,
+         body: JSON.stringify({data})
+     }
+  } catch(err){
+    console.log(err)
+  }
+  
   }
